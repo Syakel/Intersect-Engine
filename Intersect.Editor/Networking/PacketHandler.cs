71de0fc28c23bba4a3d302d12612ce6def169be8
Syakel <robert55267@gmail.com>
2025-07-12 16:06:26 -0500

    Update PacketHandler.cs

--- Intersect.Editor/Networking/PacketHandler.cs
+++ Intersect.Editor/Networking/PacketHandler.cs
@@ -15,6 +15,7 @@ using Intersect.Framework.Core.GameObjects.Maps.MapList;
 using Intersect.Framework.Core.GameObjects.NPCs;
 using Intersect.Framework.Core.GameObjects.PlayerClass;
 using Intersect.Framework.Core.GameObjects.Resources;
+using Intersect.Framework.Core.GameObjects.Skills;
 using Intersect.Framework.Core.GameObjects.Variables;
 using Intersect.GameObjects;
 using Intersect.Network;
@@ -733,6 +734,21 @@ internal sealed partial class PacketHandler
                     var pvar = new UserVariableDescriptor(id);
                     pvar.Load(json);
                     UserVariableDescriptor.Lookup.Set(id, pvar);
+                }
+
+                break;
+
+            case GameObjectType.Skill:
+                if (deleted)
+                {
+                    var skill = SkillDescriptor.Get(id);
+                    skill.Delete();
+                }
+                else
+                {
+                    var skill = new SkillDescriptor(id);
+                    skill.Load(json);
+                    SkillDescriptor.Lookup.Set(id, skill);
                 }
 
                 break;
